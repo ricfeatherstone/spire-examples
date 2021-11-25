191d8620c45f06b90918aa457998e5f73d0f0f4b
ricfeatherstone <ricfeatherstone@users.noreply.github.com>
2021-11-25 17:15:54 +0000

    feat: generate vault's key and cert using cfssl

---
+++ examples/vault-integration/vault/generate-certs.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+if [ ! -d bin ]; then
+  mkdir bin
+fi
+
+if [ ! -f bin/cfssl ]; then
+  curl -Lo bin/cfssl https://github.com/cloudflare/cfssl/releases/download/v1.6.1/cfssl_1.6.1_linux_amd64
+  curl -Lo bin/cfssljson https://github.com/cloudflare/cfssl/releases/download/v1.6.1/cfssljson_1.6.1_linux_amd64
+  chmod +x bin/cfssl bin/cfssljson
+fi
+
+if [ ! -f ca.pem ]; then
+  cfssl gencert -initca certs/ca-csr.json | cfssljson -bare certs/ca
+  cfssl gencert \
+      -ca=certs/ca.pem \
+      -ca-key=certs/ca-key.pem \
+      -config=certs/ca-config.json \
+      -profile=both \
+      certs/vault-csr.json | cfssljson -bare certs/vault
+fi
+
